5d96cb9d986afd1579701f0c769c380959874253
rogerHuntGauntlet <roger.b.hunt.iii@gauntletai.com>
2025-01-13 12:41:32 -0500

    ffs

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,3 +11,37 @@ export default function RootLayout({
     </html>
   )
 }
+
+export const metadata = {
+  title: 'OHF Partners',
+  description: 'Elite community platform for teams',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        sizes: '32x32',
+      },
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+        sizes: 'any'
+      },
+      {
+        url: '/apple-touch-icon.png',
+        type: 'image/png',
+        sizes: '180x180'
+      },
+    ],
+    shortcut: '/favicon.ico',
+    apple: [
+      {
+        url: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
+  themeColor: '#000000',
+  viewport: 'width=device-width, initial-scale=1',
+}
